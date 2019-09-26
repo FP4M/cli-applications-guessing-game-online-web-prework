f4b84847adffcd01bd071162f527678fa243d693
Prince Charles <princecharles35@yahoo.com>
2019-09-26 13:29:30 +0000

    Automatically backed up by Learn

--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -3,3 +3,4 @@ require_relative "../guessing_game_cli"
 run_guessing_game
 rand #=> Returns a random float like 0.39113137693072575
 rand(20) #=> Returns a random integer from 0 to 19
+rand(20) + 1 #=> Returns a random integer from 1 to 20
