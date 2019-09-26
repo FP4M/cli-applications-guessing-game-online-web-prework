09408a5c150c21115542fdcbf74e66e5fba5135f
Prince Charles <princecharles35@yahoo.com>
2019-09-26 13:30:28 +0000

    Automatically backed up by Learn

--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -5,3 +5,7 @@ rand #=> Returns a random float like 0.39113137693072575
 rand(20) #=> Returns a random integer from 0 to 19
 rand(20) + 1 #=> Returns a random integer from 1 to 20
 input = gets
+2.6.1 :001 > input = gets
+hello
+ => "hello\n"
+ 
